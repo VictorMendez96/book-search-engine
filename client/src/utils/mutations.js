706625d9778ebbdfeb62ebe43bb12b527252eca3
V Mendez <victormendez990@gmail.com>
2022-10-24 04:15:11 -0600

    Add to mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,17 +1,35 @@
 import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
+    mutation loginUser() {
 
+    }
 `;
 
 export const ADD_USER = gql`
-
+    mutation addUser() {
+        addUser(input: $input) {
+            token
+            user {
+            
+            }
+        }
+        
+    }
 `;
 
 export const SAVE_BOOK = gql`
+    mutation saveBook() {
+        saveBook(input: $input) {
 
+        }
+    }
 `;
 
 export const REMOVE_BOOK = gql`
-
+    mutation removeBook($key: String!) {
+        removeBook(key: $key) {
+            
+        }
+    }
 `;
